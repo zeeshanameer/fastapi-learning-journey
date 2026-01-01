f6f593153d30551adcbbda953f33fff52b4b97d6
zeeshanameer <42909@students.riphah.edu.pk>
2026-01-01 23:00:06 +0500

    HTTP First Method

--- main.py
+++ main.py
@@ -1,7 +1,22 @@
 from fastapi import FastAPI
+import json
 
 app = FastAPI()
 
+def load_data():
+    with open("patients.json", "r") as file:
+        data = json.load(file)
+    return data
+
 @app.get("/")
 def read_root():
-    return {"message": "Hello, FastAPI Journey!"}
+    return {"message": "Hello, This is the main application!"}
+
+@app.get("/About")
+def about():
+    return {"message": "Hello, This is the About page of the main application!"}
+
+@app.get("/view")
+def view():
+    data = load_data()
+    return data
